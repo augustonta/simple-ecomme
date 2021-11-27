2656a9abe8d99fe846482449e5ed3d4065047161
root <root@ip-172-31-94-86.ec2.internal>
2021-11-27 02:45:22 +0000

    subo codigo con php.ini y bd configurada

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+	ini_set('display_errors',1);
+	error_reporting(-1);
+	define('DB_HOST', 'mysqlecomme.ci0kai26u4we.us-east-1.rds.amazonaws.com');
+    define('DB_USER', 'admin');
+    define('DB_PASSWORD', 'adminadmin');
+    define('DB_DATABASE', 'ecome');
+?>
