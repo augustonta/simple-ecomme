5d15ac80fdb9a2cca09853980123126ac0fac7a3
root <root@ip-172-31-8-166.ec2.internal>
2021-11-29 23:43:35 +0000

    cambio config.php con env

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 	ini_set('display_errors',1);
 	error_reporting(-1);
-	define('DB_HOST', 'mysqlecomme.ci0kai26u4we.us-east-1.rds.amazonaws.com');
+	define('DB_HOST', $_ENV["DBHOST"] );
     define('DB_USER', 'admin');
     define('DB_PASSWORD', 'adminadmin');
     define('DB_DATABASE', 'ecome');
